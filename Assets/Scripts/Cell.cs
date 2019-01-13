486fa516fc33d783edb44f21dead6b8e5767cdc3
GehadAbdElhalim <halimgego@gmail.com>
2019-01-14 00:04:50 +0200

    first commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Terrain
+{
+    Grass, Dirt, Water
+};
+
+public enum Effect
+{
+    Fire, Thunder, NoEffect
+};
+
+public class Cell
+{
+    Terrain terrain;
+    Effect effect;
+
+    public Cell(Terrain t, Effect e)
+    {
+        this.terrain = t;
+        this.effect = e;
+    }
+
+    public void ApplyEffect()
+    {
+
+    }
+
+    public void getEffect()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[Tooltip("The speed of the character")]
+	public float MovementSpeed;
+
+	private Animator anim;
+
+	private bool playerMoving;
+
+	private Vector2 lastMove;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerMoving = false;
+
+		if (Input.GetAxisRaw("Horizontal") >= 0.5 || Input.GetAxisRaw("Horizontal") <= -0.5){
+			transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal")*MovementSpeed*Time.deltaTime,0f));
+			playerMoving = true;
+			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"),0f);
+		}
+		if (Input.GetAxisRaw("Vertical") >= 0.5 || Input.GetAxisRaw("Vertical") <= -0.5){
+			transform.Translate(new Vector2(0f,Input.GetAxisRaw("Vertical")*MovementSpeed*Time.deltaTime));
+			playerMoving = true;
+			lastMove = new Vector2(0f,Input.GetAxisRaw("Vertical"));
+		}
+
+		anim.SetFloat("MoveX",Input.GetAxisRaw("Horizontal"));
+		anim.SetFloat("MoveY",Input.GetAxisRaw("Vertical"));
+		anim.SetBool("PlayerMoving",playerMoving);
+		anim.SetFloat("LastMoveX",lastMove.x);
+		anim.SetFloat("LastMoveY",lastMove.y);
+	}
+}
